88c36e7ec70c0bc121b91cc2300c5cdff6610fa8
Aditya Sharma <adityasharma626367@gmail.com>
2025-07-22 20:50:26 +0530

    feat: enhance ContactUs page layout and input styling for better responsiveness

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from "react";
 import { FiPhoneCall } from "react-icons/fi";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { MdPlace } from "react-icons/md";
 
-
-
-
 function ContactUs() {
   return (
-    <div className="w-full flex p-4 pt-20 lg:px-15 h-screen bg-black text-white gap-2">
-      <div className="w-1/2 h-full ">
-        <div className="flex flex-col justify-center w-full h-full gap-10">
-          <div className="">
-            <h1 className="text-4xl font-bold uppercase mb-2">
+    <div className="w-full flex md:flex-row flex-col p-4 pt-20 justify-center lg:px-15 lg:h-screen bg-black text-white gap-2">
+      <div className="lg:w-1/2 w-full h-full ">
+        <div className="flex flex-col justify-center  w-full h-full gap-10">
+          <div className=" w-full">
+            <h1 className="lg:text-4xl text-3xl font-bold uppercase mb-2 ">
               Get in touch with us
             </h1>
-            <p className="text-lg w-120">
+            <p className="lg:text-lg text-sm lg:w-120 w-100   ">
               Got any questions? We are here to help you. Chat to our customer
               team 24/7 and get your issues sorted effortlessly.
             </p>
@@ -57,30 +54,30 @@ function ContactUs() {
         </div>
       </div>
 
-      <div className="w-1/2 h-full flex items-center justify-center ">
-        <div className="flex flex-col justify-center items-start gap-5">
+      <div className="lg:w-1/2 h-full flex items-center md:justify-center mt-5 sm:mt-0">
+        <div className="flex flex-col justify-center items-center gap-5 w-full">
           <input
-            className="px-5 py-2 outline-none border-1 rounded-xl text-xl w-100 "
+            className="px-5 py-2 outline-none border-1 rounded-xl lg:text-xl lg:w-100 md:w-80 w-full "
             type="text"
             placeholder="Full Name"
           />
           <input
-            className="px-5 py-2 outline-none border-1 rounded-xl text-xl w-100 "
-            type="text"
+            className=" px-5 py-2 outline-none border-1 rounded-xl lg:text-xl lg:w-100 md:w-80 w-full "
+            type="email"
             placeholder="Email"
           />
           <input
-            className="px-5 py-2 outline-none border-1 rounded-xl text-xl w-100 "
-            type="text"
+            className=" px-5 py-2 outline-none border-1 rounded-xl lg:text-xl lg:w-100 md:w-80 w-full"
+            type="number"
             placeholder="Phone no."
           />
           <textarea
-            className="w-full h-40 p-2 px-3 rounded-xl border-1 outline-0 "
+            className=" h-40 p-2 px-3 rounded-xl border-1 outline-0 lg:text-xl lg:w-100 md:w-80 w-full"
             name=""
             id=""
             placeholder="Write Something..."
           ></textarea>
-          <button className="w-full bg-green-400 text-black py-3 text-xl font-semibold rounded-xl">
+          <button className="w-full bg-green-400 text-black py-3 text-xl font-semibold rounded-xl lg:w-100 md:w-80">
             Ask For Call?
           </button>
         </div>
@@ -89,4 +86,4 @@ function ContactUs() {
   );
 }
 
-export default ContactUs
+export default ContactUs;
